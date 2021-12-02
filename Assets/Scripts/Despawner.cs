fc6dbe5201340d932e5c233840fc77211aef8956
axell151 <axellharold15@gmail.com>
2021-12-02 12:56:59 +0700

    update

--- Assets/Scripts/Despawner.cs
+++ Assets/Scripts/Despawner.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Lean.Pool;
 public class Despawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Jalan"))
+        {
+            Debug.Log($"Despawn{other.name}");
+            LeanPool.Despawn(other.gameObject);
+        }
     }
 }
