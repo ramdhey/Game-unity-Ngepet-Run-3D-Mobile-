5c0ae1c6e4ed708c63cfa11dab89f629327c47e6
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2022-01-12 22:44:53 +0700

    upd

--- Assets/Scripts/music.cs
+++ Assets/Scripts/music.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class music : MonoBehaviour
 {
     public static music Instance { get; set; }
 
-    
+    public Slider slidervolume;
 
     private void Awake()
     {
@@ -19,6 +20,32 @@ public class music : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musik"))
+        {
+            PlayerPrefs.SetFloat("musik", 1);
+            Load();
+        }
+
+    }
+
+   public void ubahvolume()
+    {
+        AudioListener.volume = slidervolume.value;
+        save();
+    }
+
+    private void Load()
+    {
+        slidervolume.value = PlayerPrefs.GetFloat("musik");
+    }
+
+    private void save()
+    {
+        PlayerPrefs.SetFloat("musik", slidervolume.value);
     }
 
 }
